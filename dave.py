0e046d706cc8b199b2a443caa89f9f9a1a82cdb1
fisherthewol <george.kokinis@gmail.com>
2017-06-06 19:28:54 +0100

    Update dave.py
    Add !prequel

--- dave.py
+++ dave.py
@@ -1,44 +1,81 @@
-import discord, feedparser, datetime
+import discord, feedparser, datetime, praw,
 from discord.ext import commands
 
 now = datetime.datetime.now()
 h = now.hour
-description = "This is a WIP bot to replace newsreaders."
+description = "This is a WIP bot to work discord."
 bot_prefix = "!"
 client = commands.Bot(description=description, command_prefix=bot_prefix)
 
-@client.event
-async def on_ready():
-    print("Login Successful")
-    print("Name : {}" .format(client.user.name))
-    print("ID : {}" .format(client.user.id))
+reddit = praw.Reddit('prequelbot',
+                     user_agent='prequelbot:v{}:t3rr0r_f3rr3t'.format(__version__))
+subreddit = reddit.subreddit("prequelmemes")
+global a
+a = 0
+global topattr
+topattr = {"title": "", "img": "", "id": ""}
 
-@client.command(pass_context=True)
-async def helpnews(ctx):
-    print("!helpnews")
-    await client.say("\nAvailible commands:\n"
-                     "!helpnews -- What you're seeing now.\n"
-                     "!news         -- See top news stories now.\n")
 
-@client.command(pass_context=True)
-async def news(ctx):
-    print("!news")
-    bbc = feedparser.parse("https://www.reddit.com/domain/bbc.com/"
-                           "top/.rss")
-    game = feedparser.parse("https://www.gameinformer.com/b/mainfeed"
-                            ".aspx?Tags=feature")
-    await client.say(bbc.entries[0]['link'])
-    await client.say(game.entries[0]['link'])
+def prawin():
+    topsub = subreddit.top("day", limit=1)
+    oldsub = {"title": "", "img": "", "id": ""}
+    topattr = {"title": "", "img": "", "id": ""}
+    global a
+    if a == 0:
+        for submission in topsub:
+            oldsub["title"] = str(submission.title)
+            oldsub["img"] = str(submission.url)
+            oldsub["id"] = str(submission.id)
+        logger("init, returning top now")
+        print("init, returning top now")
+        return oldsub
 
-#@client.event
-#async def timer():
-#    while True:
-#        if(h == 9) or (h == 18) or (h == 0) or (h == 3):
-#            bbc = feedparser.parse("https://www.reddit.com/domain/bbc.com/"
-#                                   "top/.rss")
-#            game = feedparser.parse("https://www.gameinformer.com/b/mainfeed"
-#                                    ".aspx?Tags=feature")
-#            await client.say(bbc.entries[0]['link'])
-#            await client.say(game.entries[0]['link'])
 
-client.run("")
+def discout():
+    @client.event
+    async def on_ready():
+        print("Login Successful")
+        print("Name : {}" .format(client.user.name))
+        print("ID : {}" .format(client.user.id))
+
+    @client.command(pass_context=True)
+    async def help(ctx):
+        print("!help")
+        await client.say("\nAvailible commands:\n"
+                         "!help -- What you're seeing now.\n"
+                         "!news -- See top news stories now.\n"
+                         "!prequel -- See top post from /r/prequelmemes.\n")
+
+    @client.command(pass_context=True)
+    async def news(ctx):
+        print("!news")
+        bbc = feedparser.parse("https://www.reddit.com/domain/bbc.com/"
+                               "top/.rss")
+        game = feedparser.parse("https://www.gameinformer.com/b/mainfeed"
+                                ".aspx?Tags=feature")
+        await client.say(bbc.entries[0]['link'])
+        await client.say(game.entries[0]['link'])
+
+    @client.command(pass_context=True)
+    async def prequel(ctx):
+        print("!prequel")
+        # Always get top post at time.
+        global a
+        a = 0
+        topbot = prawin()
+        await client.say(" \nImage: {}\nTitle = {}\nComments = "
+                         "https://redd.it/{}\n".format(
+                         topbot["img"],topbot["title"],topbot["id"]))
+
+    client.run("inserthere")
+
+
+if __name__ == "__main__":
+    logger("Main File; discout is being called.")
+    print("Main File; discout is being called.")
+    try:
+        discout()
+    except:
+        logger("Error while calling/in main(); exiting.")
+        print("Error while calling/in main(); exiting.")
+        sys.exit()
