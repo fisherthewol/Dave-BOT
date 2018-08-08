db5506d9d0a753edb2a9c5f8122d8ca4c8ffbbe5
Fisher <george.kokinis@gmail.com>
2018-08-08 21:57:01 +0100

    Laid framework for using config parser.
    Now just need to actually do it.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import configparser
 import os
 
 from DaveBOT import core
@@ -6,25 +7,35 @@ from DaveBOT import core
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t",
+    parser.add_argument("-co",
+                        "--configorigin",
+                        help="specify where to find config; "
+                             "defaults to envrionment",
+                        choices=["object", "environ"],
+                        type=str)
+    parser.add_argument("-tk",
                         "--token",
                         help="client token for discord",
                         type=str)
     parser.add_argument("-id",
                         "--adminid",
-                        help="Discord userid of the bot's admin.")
+                        help="Discord userid of the bot's admin.",
+                        type=str)
     parser.add_argument("-v4",
-                        help="force ipv4 (instead of v6)",
+                        help="force ipv4 (instead of v4/v6)",
                         action="store_true")
     args = parser.parse_args()
 
-    if args.token:
-        os.environ["token"] = args.token
-    elif not os.environ.get("token"):
-        raise RuntimeError("No discord token found.")
+    if args.configorigin == "object":
+        pass
+    else:
+        if args.token:
+            os.environ["token"] = args.token
+        elif not os.environ.get("token"):
+            raise RuntimeError("No discord token found.")
 
-    if args.adminid:
-        os.environ["adminid"] = args.admin
+        if args.adminid:
+            os.environ["adminid"] = args.admin
 
     bot = core.Dave(cc, adid, ll, rid, rsc, wk)
     bot.discout()
